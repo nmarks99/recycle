7e1dca5086dcb4b01eca3282c20bfe0c124b1dad
Nick <nmarks99@gmail.com>
2022-06-14 00:52:04 -0500

    Save

---
+++ save.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+git add --all && git commit -m"Save" && git push
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
-use std::fs;
-use filetime::FileTime;
+// use std::fs;
+// use filetime::FileTime;
+use std::env;
+
+fn extra() {
+    println!("extra");
+}
+
+fn single() {
+   println!("single") 
+}
 
 
 fn main() {
+   
+    let args: Vec<String> = env::args().collect();
     
-    let metadata = fs::metadata("foo.txt").unwrap();
-
-    let mtime = FileTime::from_last_modification_time(&metadata);
-
-    println!("{}",mtime.unix_seconds());
-
+    match args.len() {
+        1 => panic!("re: Missing operand"),
+        2 => single(),
+        3 => extra(),
+        _ => println!("Not implemented"),
+    }
+    
+    // let ref filename = &args[1];
+    // let metadata = fs::metadata("foo.txt").unwrap();
+    // let unix_time = FileTime::from_last_modification_time(&metadata).unix_seconds();
 }
