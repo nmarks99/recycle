d4cf2325bad354c7b86dc01bb823ec5b43e02e0d
Nick <nmarks99@gmail.com>
2022-06-14 13:52:05 -0500

    basic functionality is done

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,6 @@ use std::process::Command;
 use std::path::Path;
 
 
-//
-// fn extra() {
-    // println!("extra");
-// }
-//
-
 fn sys_command(command:String) -> std::process::Output {
 
     // See if on windows
@@ -38,26 +32,10 @@ fn sys_command(command:String) -> std::process::Output {
     return output;
 }
 
-fn is_trash_dir(rec_path: &str) {
-   if !Path::exists(Path::new(rec_path)) {
-       let cmd = format!("mkdir {}",rec_path);
-       sys_command(cmd);
-   }
-}
-
-
-fn move_to_trash(filename: &str) {
-     
-    // Path to the recycle bin
-    let rec_path: &str = "./trash";
+fn move_to_trash(filename: &str, rec_path: &str) {
     
     // Check that the given file exists
-    if !Path::new(filename).exists() {
-        panic!("{} does not exist",filename);
-    }
-
-    // Create the recycle bin if it doesn't exist
-    is_trash_dir(rec_path);
+    assert!(Path::new(filename).exists(),"{} does not exist",filename);
 
     // Generate the mv command
     let full_command = format!("mv {} {}",filename, rec_path);
@@ -66,17 +44,30 @@ fn move_to_trash(filename: &str) {
     sys_command(full_command);
 }
 
-
+// fn delete_old(filename: &str) {
+    //
+// }
 
 fn main() {
-   
+    
+    // Path to the recycle bin
+    let rec_path: &str = "~/.trash/";
+    
+    // Create the recycle bin if it doesn't exist
+    if !Path::exists(Path::new(rec_path)) {
+       let cmd = format!("mkdir {}",rec_path);
+       sys_command(cmd);
+    } 
+    
+    // Get command line arguments
     let args: Vec<String> = env::args().collect();
-    let ref filename = &args[1]; 
-    match args.len() {
-        1 => panic!("re: Missing operand"),
-        2 => move_to_trash(filename),
-        _ => println!("Too many arguments. Expected 1, recieved {}",args.len()),
+    assert!(args.len() >= 2, "Not enough input arguments");
+
+    // Move requested files to the recycle bin 
+    for i in 1..args.len() {
+        move_to_trash(&args[i][..],rec_path);
     }
+    
     
     // let ref filename = &args[1];
     // let metadata = fs::metadata("foo.txt").unwrap();
